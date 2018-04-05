ca614c7f09b92b65577a5d05817503a1011b78ba
yoshirolas <yoshirolas@tut.by>
2018-04-05 15:18:37 +0300

    Commented some unnecessary strings for development mode

--- src/services/getApiPrefix.js
+++ src/services/getApiPrefix.js
@@ -1,12 +1,5 @@
-const port = process.env.PORT || 3001;
 const serverOrigin = '' + window.location.origin;
-const host = serverOrigin.slice(0, -serverOrigin.indexOf(':') - 1);
-let apiPrefix;
-if (host.length > 5) {
-  apiPrefix = `${ host }:${ port }`;
-} else {
-  apiPrefix = serverOrigin;
-}
+const apiPrefix = serverOrigin;
 
 
 export default apiPrefix;
